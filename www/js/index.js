df445d773f4111f5557a242163ec390d217fbd15
soronto3603 <soronto3603@gmail.com>
2018-01-29 18:47:09 +0900

    firstcommit

---
+++ www/js/index.js
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var content=document.getElementById('content');
+function build_message(msg){
+    var html='<div class=line>';
+    html+='<div class="right-chat bubble">'+msg+'<div class=date>2018/01/29</div></div>';
+    html+='</div>';
+    return html;
+}
+function send_message(){
+    var msg=document.getElementById('key_input_text').value;
+    content.innerHTML=content.innerHTML+build_message(msg);
+}
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
