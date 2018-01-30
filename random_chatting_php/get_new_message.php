28f7db5c094963c55b27afbb7059da87ec347432
soronto3603 <soronto3603@gmail.com>
2018-01-31 00:49:12 +0900

    issue finish about get_new_message.php

--- random_chatting_php/get_new_message.php
+++ random_chatting_php/get_new_message.php
@@ -1,13 +1,21 @@
 <?php
     include("init.php");
     $id=$_GET['id'];
-    $query="SELECT * FROM rc_message WHERE src_user=$id OR dest_user=$id";
-    
     $result_array=array();
     $flag=-1;
+    $query="SELECT * FROM rc_message WHERE src_user=$id";
     if($result=mysqli_query($con,$query)){
         while($row=mysqli_fetch_row($result)){
             $query_2="UPDATE rc_message SET src_user=-1 WHERE no=".$row[0];
+            mysqli_query($con,$query_2);
+            array_push($result_array,$row);
+            $flag=1;
+        }
+    }
+    $query="SELECT * FROM rc_message WHERE dest_user=$id";
+    if($result=mysqli_query($con,$query)){
+        while($row=mysqli_fetch_row($result)){
+            $query_2="UPDATE rc_message SET dest_user=-1 WHERE no=".$row[0];
             mysqli_query($con,$query_2);
             array_push($result_array,$row);
             $flag=1;
